4fb83944948a92a25c851232f6ee00805b9b3054
rude-dev <69910356+rude-dev@users.noreply.github.com>
2025-04-11 02:42:29 +0530

    solved task 1

---
+++ Answers_AdityaPrasad/Readme.md
@@ -0,0 +1,4 @@
+Name - Aditya Prasad
+Registation number - 241040002
+Branch - Production
+email id - avprasad_b24@pe.vjti.ac.in
--- Answers_AdityaPrasad/task1.py
+++ Answers_AdityaPrasad/task1.py
@@ -1,15 +1,18 @@
 import cv2
-import numpy as np
 
-img = cv2.imread("C:/Users/adity/OneDrive/Documents/GitHub/Xplore2.0/Answers_AdityaPrasad/Task1.png", 0)
-# wasn't able to get image by just its name so used the full path
+img = cv2.imread("C:/Users/adity/OneDrive/Desktop/Task1.png")
+count = 0 
 
-kernel = np.ones((5, 5), np.uint8)
-# got the kernel from chatgpt
+size = img.shape
+height = size[0]
+width = size[1]
 
-closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+for i in range(height):
+    for j in range(width):
+        if img[i][j][0] != 255 and img[i][j][1] != 255 and img[i][j][2] != 255:
+            print(f"Pixel at {i} , {j} is not white")
+            count += 1
+            
 
-
-cv2.imshow("Task 1", closed)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print("The number of non white pixels in the image is: ", count)
+    

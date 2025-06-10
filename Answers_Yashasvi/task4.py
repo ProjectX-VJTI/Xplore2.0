9b4be2a61bf3dd6dc95ac776b3fbe16af1afa7ba
Star-Light-9 <lighteningstar629@gmail.com>
2025-06-10 19:13:32 +0530

    Updated task4.py

--- Answers_Yashasvi/task4.py
+++ Answers_Yashasvi/task4.py
@@ -1,9 +1,3 @@
-import cv2 as cv
-from google.colab.patches import cv2_imshow
-
-from google.colab import files
-uploaded = files.upload()
-
 # Import image into variable img
 img4 = cv.imread("Task4.png")
 
@@ -13,6 +7,9 @@ gray = cv.cvtColor(img4, cv.COLOR_BGR2GRAY)
 # Threshold to binary (black and white)
 _, thresh = cv.threshold(gray, 240, 255, cv.THRESH_BINARY_INV)
 
+# Save height and width for traversal
+height, width = img4.shape[:2]
+
 # Counters
 c, side = 0, 0
 
@@ -20,11 +17,11 @@ c, side = 0, 0
 for i in range(height):
   c=0
   for j in range(width):
-    if thresh[i,j]==0:
+    if thresh[i,j]==255:
       c+=1
       if c > side :
         side = c
-    elif thresh[i,j]==255:
+    elif thresh[i,j]==0:
       c=0
 
 print(f"Area of largest square : {side*side}")
